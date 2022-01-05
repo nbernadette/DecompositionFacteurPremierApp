c7c818b8872d34e99e497dae3269a1eb12171015
unknown <aurorembey@gmail.com>
2022-01-05 18:41:22 +0100

    Inteface de base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'factprem';
+
+  input: FormControl;
+  primeNumbers: number[];
+  stringToPrint: string;
+  constructor(private formBuilder: FormBuilder){
+    this.primeNumbers = [];
+    this.input = formBuilder.control(0,[Validators.required]);
+    this.stringToPrint = '';
+  }
+
+  submitNumber(){
+    this.stringToPrint = ''
+    var chiffre = this.input.value;
+    console.log(chiffre);
+    
+    this.primeNumbers = this.getPrimeNumber(chiffre);
+
+    if(this.primeNumbers.length >= 0){
+      // Construire la chaine des nombres à afficher avec les signes de multiplication
+      this.primeNumbers.forEach((value) => this.stringToPrint += value.toString() + '*');
+      
+      // Supprimer le dernier *
+      this.stringToPrint = this.stringToPrint.substring(0,this.stringToPrint.length-1);
+    }
+  }
+
+  getPrimeNumber(n: number): number[]{
+    return [];
+  }
 }
